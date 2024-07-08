dc379959c5850557433d5395bc0563469f7b12e7
Piladda <107570496+YuyPPY@users.noreply.github.com>
2024-07-08 21:18:51 +0700

    updata dashboard

---
+++ app/(front-end)/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+ <div className="flex items-center justify-center  flex-col min-h-screen ">
+  <h2 className="text-4xl">
+    Welcome to  Ecommerce BG
+  </h2>
+ </div>
+  );
+}
